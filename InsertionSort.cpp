6d5acb6b45e4ea2fdeb47efee6ce4746ff0df3e4
Himanshu Yadav <120887356+AsherKingpin@users.noreply.github.com>
2026-01-19 13:47:10 +0530

    Implement Insertion Sort algorithm in C++

---
+++ InsertionSort.cpp
@@ -0,0 +1,32 @@
+#include <algorithm>
+#include <map>
+#include <set>
+#include <vector>
+#include <iostream>
+using namespace std;
+
+void insertion_sort(int arr[], int n){
+    for(int i = 1; i < n ; i++){
+        int j = i;
+        while(j > 0 && arr[j -1] > arr[j]){
+            swap(arr[j], arr[j -1]);
+            j--;
+        }
+    }
+}
+
+int main(){
+    int n;
+    cout << "Enter the length of the array: ";
+    cin >> n;
+    int arr[n];
+    for(int i = 0; i < n; i++){
+        cin >> arr[i];
+        cout << endl;
+    }
+    insertion_sort(arr,n);
+    for(int i = 0; i < n; i++){
+        cout << arr[i] << " ";
+    }
+    return 0;
+}
