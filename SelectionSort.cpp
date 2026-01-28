4b2b22120bcab0cdc9bac9f7ad57872b6b5d4b52
Himanshu Yadav <120887356+AsherKingpin@users.noreply.github.com>
2026-01-28 15:45:23 +0530

    Update SelectionSort.cpp

--- SelectionSort.cpp
+++ SelectionSort.cpp
@@ -7,7 +7,7 @@ using namespace std;
 
 void selection_sort(int arr[], int n){
     int min = 0;
-    for(int i = 0; i < n - 2; i++){
+    for(int i = 0; i < n - 1; i++){
         min = i;
         for(int j = i + 1; j < n; j++){
             if(arr[j] < arr[min]){
